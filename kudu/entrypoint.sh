e7f701afb2e8346130c003ea9fcab025c36538c8
liusheng <liusheng2048@gmail.com>
2020-01-17 17:50:29 +0800

    update

--- kudu/entrypoint.sh
+++ kudu/entrypoint.sh
@@ -1,5 +1,65 @@
 #!/bin/bash
-mkdir -p /opt/kudu/build/debug
-cd /opt/kudu/build/debug
-cmake ../..
-make -j4
+
+#cd /opt/kudu
+#git pull
+
+case $1 in
+lint)
+  mkdir -p /opt/results/lint/
+  mkdir -p /opt/kudu/build/lint
+  cd /opt/kudu/build/lint
+  cmake ../.. | tee /opt/results/lint/cmake.log
+  make ilint 2>&1 | tee /opt/results/lint/lint.log
+  ;;
+iwyu)
+  mkdir -p /opt/results/iwyu/
+  mkdir -p /opt/kudu/build/iwyu
+  cd /opt/kudu/build/iwyu
+  cmake ../.. 2>&1 | tee /opt/results/iwyu/cmake.log
+  make iwyu 2>&1 | tee /opt/results/iwyu/iwyu.log
+  ;;
+debug)
+  mkdir -p /opt/results/debug/
+  mkdir -p /opt/kudu/build/debug
+  cd /opt/kudu/build/debug
+  make -j8 2>&1 | tee mkdir -p /opt/results/debug/build.log
+  ctest -j8 2>&1 | tee /opt/results/debug/test_console.log
+  cp -r /opt/kudu/build/debug/test-logs/ /opt/results/debug/
+  ;;
+release)
+  mkdir -p /opt/results/release/
+  mkdir -p /opt/kudu/build/release
+  cd /opt/kudu/build/release
+  ../../build-support/enable_devtoolset.sh \
+  ../../thirdparty/installed/common/bin/cmake \
+  -DCMAKE_BUILD_TYPE=release ../..
+  make -j8 2>&1 | tee mkdir -p /opt/results/release/build.log
+  ;;
+asan)
+  mkdir -p /opt/results/asan/
+  mkdir -p /opt/kudu/build/asan
+  cd /opt/kudu/build/asan
+  CC=../../thirdparty/clang-toolchain/bin/clang \
+  CXX=../../thirdparty/clang-toolchain/bin/clang++ \
+  ../../thirdparty/installed/common/bin/cmake \
+  -DKUDU_USE_ASAN=1 ../..
+  make -j8 2>&1 | tee /opt/results/asan/build.log
+  ctest -j8 2>&1 | tee /opt/results/asan/test_console.log
+  cp -r /opt/kudu/build/asan/test-logs/ /opt/results/asan/
+  ;;
+tsan)
+  mkdir -p /opt/results/tsan/
+
+  mkdir -p /opt/kudu/build/tsan
+  cd /opt/kudu/build/tsan
+  CC=../../thirdparty/clang-toolchain/bin/clang \
+  CXX=../../thirdparty/clang-toolchain/bin/clang++ \
+  ../../thirdparty/installed/common/bin/cmake \
+  -DKUDU_USE_TSAN=1 ../..
+  make -j8 2>&1 | tee /opt/results/tsan/build.log
+  ctest -j8 2>&1 | tee /opt/results/tsan/test_console.log
+  cp -r /opt/kudu/build/tsan/test-logs/ /opt/results/tsan/
+  ;;
+*)
+  exec $@
+esac
