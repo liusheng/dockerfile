d865d2b4a9c6afd5c8b22d24b2a37b67c37d2b0b
liusheng <liusheng2048@gmail.com>
2020-02-18 17:40:15 +0800

    update

--- kudu-aarch64/entrypoint.sh
+++ kudu-aarch64/entrypoint.sh
@@ -1,62 +1,41 @@
 #!/bin/bash -ex
-#cd /opt/kudu
-#git pull
+cd /opt/kudu
+git remote add upstream https://github.com/apache/kudu
+git fetch upstream master
+git fetch origin aarch64-support
+git config --global user.email "nobody@example.com"
+git config --global user.name "nobody"
+git rebase upstream/master
 
 case $1 in
 lint)
-  mkdir -p /opt/results/lint/
-  mkdir -p /opt/kudu/build/lint
-  cd /opt/kudu/build/lint
-  cmake ../.. | tee -a /opt/results/lint/console.log
-  make ilint | tee -a /opt/results/lint/console.log
+  mkdir -p /opt/results/lint
+  BUILD_TYPE="LINT" bash -x build-support/jenkins/build-and-test.sh 2>&1 | tee -a /opt/results/lint/console.log
+  cp -r /opt/kudu/build/lint/test-logs/ /opt/results/lint/
   ;;
 iwyu)
-  mkdir -p /opt/results/iwyu/
-  mkdir -p /opt/kudu/build/iwyu
-  cd /opt/kudu/build/iwyu
-  cmake ../.. 2>&1 | tee -a /opt/results/iwyu/console.log
-  make iwyu 2>&1 | tee -a /opt/results/iwyu/console.log
+  mkdir -p /opt/results/iwyu
+  BUILD_TYPE="IWYU" bash -x build-support/jenkins/build-and-test.sh 2>&1 | tee -a /opt/results/iwyu/console.log
+  cp -r /opt/kudu/build/iwyu/test-logs/ /opt/results/iwyu/
   ;;
 debug)
   mkdir -p /opt/results/debug
-  mkdir -p /opt/kudu/build/debug
-  cd /opt/kudu/build/debug
-  cmake ../.. | tee -a /opt/results/debug/console.log
-  make -j8 2>&1 | tee -a /opt/results/debug/console.log
-  ctest -j8 2>&1 | tee -a /opt/results/debug/console.log
+  BUILD_TYPE="DEBUG" KUDU_ALLOW_SLOW_TESTS=0 bash -x build-support/jenkins/build-and-test.sh 2>&1 |tee /opt/results/debug/console.log
   cp -r /opt/kudu/build/debug/test-logs/ /opt/results/debug/
   ;;
 release)
-  mkdir -p /opt/results/release/
-  mkdir -p /opt/kudu/build/release
-  cd /opt/kudu/build/release
-  ../../build-support/enable_devtoolset.sh \
-  ../../thirdparty/installed/common/bin/cmake \
-  -DCMAKE_BUILD_TYPE=release ../.. | tee -a /opt/results/release/console.log
-  make -j8 2>&1 | tee -a /opt/results/release/console.log
+  mkdir -p /opt/results/release
+  BUILD_TYPE="RELEASE" KUDU_ALLOW_SLOW_TESTS=0 bash -x build-support/jenkins/build-and-test.sh 2>&1 |tee /opt/results/release/console.log
+  cp -r /opt/kudu/build/release/test-logs/ /opt/results/release/
   ;;
 asan)
-  mkdir -p /opt/results/asan/
-  mkdir -p /opt/kudu/build/asan
-  cd /opt/kudu/build/asan
-  CC=../../thirdparty/clang-toolchain/bin/clang \
-  CXX=../../thirdparty/clang-toolchain/bin/clang++ \
-  ../../thirdparty/installed/common/bin/cmake \
-  -DKUDU_USE_ASAN=1 ../.. | tee -a /opt/results/asan/console.log
-  make -j8 2>&1 | tee -a /opt/results/asan/console.log
-  ctest -j8 2>&1 | tee -a /opt/results/asan/console.log
+  mkdir -p /opt/results/asan
+  BUILD_TYPE="ASAN" KUDU_ALLOW_SLOW_TESTS=0 bash -x build-support/jenkins/build-and-test.sh 2>&1 |tee /opt/results/asan/console.log
   cp -r /opt/kudu/build/asan/test-logs/ /opt/results/asan/
   ;;
 tsan)
-  mkdir -p /opt/results/tsan/
-  mkdir -p /opt/kudu/build/tsan
-  cd /opt/kudu/build/tsan
-  CC=../../thirdparty/clang-toolchain/bin/clang \
-  CXX=../../thirdparty/clang-toolchain/bin/clang++ \
-  ../../thirdparty/installed/common/bin/cmake \
-  -DKUDU_USE_TSAN=1 ../.. | tee -a /opt/results/tsan/console.log
-  make -j8 2>&1 | tee -a /opt/results/tsan/console.log
-  ctest -j8 2>&1 | tee -a /opt/results/tsan/console.log
+  mkdir -p /opt/results/tsan
+  BUILD_TYPE="TSAN" KUDU_ALLOW_SLOW_TESTS=0 bash -x build-support/jenkins/build-and-test.sh 2>&1 |tee /opt/results/tsan/console.log
   cp -r /opt/kudu/build/tsan/test-logs/ /opt/results/tsan/
   ;;
 *)
