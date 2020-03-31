4f67e635ea377cb0d863e84bf26655b4b25f2afe
liusheng <liusheng2048@gmail.com>
2020-03-31 09:36:22 +0800

    use non-user for kudu testing

--- kudu-aarch64/entrypoint.sh
+++ kudu-aarch64/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -ex
-cd /opt/kudu
+cd ~/kudu/
 
 # use latest patch
 #git config --global user.email "nobody@example.com"
@@ -18,34 +18,34 @@ ln -s /usr/include/locale.h /usr/include/xlocale.h
 
 case $1 in
 lint)
-  mkdir -p /opt/results/lint
-  BUILD_TYPE="LINT" bash -x build-support/jenkins/build-and-test.sh 2>&1 | tee -a /opt/results/lint/console.log
-  cp -r /opt/kudu/build/lint/test-logs/ /opt/results/lint/
+  mkdir -p ~/results/lint
+  BUILD_TYPE="LINT" bash -x build-support/jenkins/build-and-test.sh 2>&1 | tee -a ~/results/lint/console.log
+  cp -r ~/kudu/build/lint/test-logs/ ~/results/lint/
   ;;
 iwyu)
-  mkdir -p /opt/results/iwyu
-  BUILD_TYPE="IWYU" bash -x build-support/jenkins/build-and-test.sh 2>&1 | tee -a /opt/results/iwyu/console.log
-  cp -r /opt/kudu/build/iwyu/test-logs/ /opt/results/iwyu/
+  mkdir -p ~/results/iwyu
+  BUILD_TYPE="IWYU" bash -x build-support/jenkins/build-and-test.sh 2>&1 | tee -a ~/results/iwyu/console.log
+  cp -r ~/kudu/build/iwyu/test-logs/ ~/results/iwyu/
   ;;
 debug)
-  mkdir -p /opt/results/debug
-  BUILD_TYPE="DEBUG" KUDU_ALLOW_SLOW_TESTS=0 bash -x build-support/jenkins/build-and-test.sh 2>&1 |tee /opt/results/debug/console.log
-  cp -r /opt/kudu/build/debug/test-logs/ /opt/results/debug/
+  mkdir -p ~/results/debug
+  BUILD_TYPE="DEBUG" KUDU_ALLOW_SLOW_TESTS=0 bash -x build-support/jenkins/build-and-test.sh 2>&1 |tee ~/results/debug/console.log
+  cp -r ~/kudu/build/debug/test-logs/ ~/results/debug/
   ;;
 release)
-  mkdir -p /opt/results/release
-  BUILD_TYPE="RELEASE" KUDU_ALLOW_SLOW_TESTS=0 bash -x build-support/jenkins/build-and-test.sh 2>&1 |tee /opt/results/release/console.log
-  cp -r /opt/kudu/build/release/test-logs/ /opt/results/release/
+  mkdir -p ~/results/release
+  BUILD_TYPE="RELEASE" KUDU_ALLOW_SLOW_TESTS=0 bash -x build-support/jenkins/build-and-test.sh 2>&1 |tee ~/results/release/console.log
+  cp -r ~/kudu/build/release/test-logs/ ~/results/release/
   ;;
 asan)
-  mkdir -p /opt/results/asan
-  BUILD_TYPE="ASAN" KUDU_ALLOW_SLOW_TESTS=0 bash -x build-support/jenkins/build-and-test.sh 2>&1 |tee /opt/results/asan/console.log
-  cp -r /opt/kudu/build/asan/test-logs/ /opt/results/asan/
+  mkdir -p ~/results/asan
+  BUILD_TYPE="ASAN" KUDU_ALLOW_SLOW_TESTS=0 bash -x build-support/jenkins/build-and-test.sh 2>&1 |tee ~/results/asan/console.log
+  cp -r ~/kudu/build/asan/test-logs/ ~/results/asan/
   ;;
 tsan)
-  mkdir -p /opt/results/tsan
-  BUILD_TYPE="TSAN" KUDU_ALLOW_SLOW_TESTS=0 bash -x build-support/jenkins/build-and-test.sh 2>&1 |tee /opt/results/tsan/console.log
-  cp -r /opt/kudu/build/tsan/test-logs/ /opt/results/tsan/
+  mkdir -p ~/results/tsan
+  BUILD_TYPE="TSAN" KUDU_ALLOW_SLOW_TESTS=0 bash -x build-support/jenkins/build-and-test.sh 2>&1 |tee ~/results/tsan/console.log
+  cp -r ~/kudu/build/tsan/test-logs/ ~/results/tsan/
   ;;
 *)
   echo "NOTICE: you may need to update the code and apply the patch!"
