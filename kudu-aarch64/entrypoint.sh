26f0128950361b50df21c13a570220cef4e3983b
liusheng <liusheng2048@gmail.com>
2020-04-26 15:28:38 +0800

    update

--- kudu-aarch64/entrypoint.sh
+++ kudu-aarch64/entrypoint.sh
@@ -15,7 +15,7 @@ fi
 
 # The Java tests need 'JAVA8_HOME' env varq
 export JAVA8_HOME=/usr/lib/jvm/java-8-openjdk-arm64
-sudo ln -s /usr/include/locale.h /usr/include/xlocale.h
+sudo ln -s /usr/include/locale.h /usr/include/xlocale.h || true
 export TEST_TIMEOUT_SECS=1800
 
 case $1 in
