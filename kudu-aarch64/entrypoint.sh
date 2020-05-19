2d4201f399e6450b225255c4258e96ba2a850924
liusheng <liusheng2048@gmail.com>
2020-05-19 11:27:23 +0800

    update kudu docker entrypoint

--- kudu-aarch64/entrypoint.sh
+++ kudu-aarch64/entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/bash -ex
+set -xo pipefail
+
 base_dir=/opt
 [ -d ${base_dir}/kudu ] && cd ${base_dir}/kudu/
 
@@ -13,39 +15,31 @@ export JAVA8_HOME=/usr/lib/jvm/java-8-openjdk-arm64
 sudo ln -s /usr/include/locale.h /usr/include/xlocale.h || true
 export TEST_TIMEOUT_SECS=1800
 
+mkdir -p ${base_dir}/results/$1
 case $1 in
 lint)
-  mkdir -p ${base_dir}/results/lint
   BUILD_TYPE="LINT" bash -x build-support/jenkins/build-and-test.sh 2>&1 | tee -a ${base_dir}/results/lint/console.log
-  cp -r ${base_dir}/kudu/build/lint/test-logs/ ${base_dir}/results/lint/
   ;;
 iwyu)
-  mkdir -p ${base_dir}/results/iwyu
   BUILD_TYPE="IWYU" bash -x build-support/jenkins/build-and-test.sh 2>&1 | tee -a ${base_dir}/results/iwyu/console.log
-  cp -r ${base_dir}/kudu/build/iwyu/test-logs/ ${base_dir}/results/iwyu/
   ;;
 debug)
-  mkdir -p ${base_dir}/results/debug
   BUILD_TYPE="DEBUG" KUDU_ALLOW_SLOW_TESTS=0 bash -x build-support/jenkins/build-and-test.sh 2>&1 |tee ${base_dir}/results/debug/console.log
-  cp -r ${base_dir}/kudu/build/debug/test-logs/ ${base_dir}/results/debug/
   ;;
 release)
-  mkdir -p ${base_dir}/results/release
   BUILD_TYPE="RELEASE" KUDU_ALLOW_SLOW_TESTS=0 bash -x build-support/jenkins/build-and-test.sh 2>&1 |tee ${base_dir}/results/release/console.log
-  cp -r ${base_dir}/kudu/build/release/test-logs/ ${base_dir}/results/release/
   ;;
 asan)
-  mkdir -p ${base_dir}/results/asan
   BUILD_TYPE="ASAN" KUDU_ALLOW_SLOW_TESTS=0 bash -x build-support/jenkins/build-and-test.sh 2>&1 |tee ${base_dir}/results/asan/console.log
-  cp -r ${base_dir}/kudu/build/asan/test-logs/ ${base_dir}/results/asan/
   ;;
 tsan)
-  mkdir -p ${base_dir}/results/tsan
   BUILD_TYPE="TSAN" KUDU_ALLOW_SLOW_TESTS=0 bash -x build-support/jenkins/build-and-test.sh 2>&1 |tee ${base_dir}/results/tsan/console.log
-  cp -r ${base_dir}/kudu/build/tsan/test-logs/ ${base_dir}/results/tsan/
   ;;
 *)
   echo "NOTICE: you may need to update the code and apply the patch!"
   exec "$@"
   ;;
 esac
+exit_code=$?
+cp -r ${base_dir}/kudu/build/$1/test-logs/ ${base_dir}/results/$1/
+exit $exit_code
