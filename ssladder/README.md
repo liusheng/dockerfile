0e094c0c7b0feadd6abb5309189e595a33c88a8f
liusheng <liusheng2048@gmail.com>
2020-08-06 15:22:16 +0800

    update ladder readme

---
+++ ssladder/README.md
@@ -0,0 +1,5 @@
+## My ladder
+
+```shell script
+docker run -p 9527:9527 -e password="*******" -d liusheng2048/sslader
+```
--- ssladder/entrypoint.sh
+++ ssladder/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ "$1" = "" ]]; then
-  ssserver -p "${port:-10111}" -k "${password:-ladder@19885}" -m "${method:-chacha20-ietf-poly1305}" -d start
+  ssserver -p "${port:-10111}" -k "${password:-ladder@19885}" -m "${method:-chacha20-ietf-poly1305}" start
 else
   exec "$@"
 fi
