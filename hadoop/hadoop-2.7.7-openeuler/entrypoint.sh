e29b9d51e277b0a2a5eaf33bf9044c08d40ed027
liusheng <liusheng2048@gmail.com>
2020-10-19 15:37:17 +0800

    update hadoop 2.7.7 openeuler job

--- hadoop/hadoop-2.7.7-openeuler/entrypoint.sh
+++ hadoop/hadoop-2.7.7-openeuler/entrypoint.sh
@@ -3,7 +3,9 @@ export JAVA_HOME=$(dirname $(dirname $(readlink -f $(which javac))))
 
 case $1 in
 test)
+  [ ! -d ~/hadoop ] && git clone https://github.com/kunpengcompute/hadoop -b release-2.7.7-aarch64
   cd ~/hadoop/
+  git pull
   mvn test -B -e -fn | sudo tee ~/hadoop-results/hadoop_all_test.log
   ;;
 *)
