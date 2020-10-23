9799e2566dc402e3e757d9ea82ebff26adca8cc8
liusheng <liusheng2048@gmail.com>
2020-10-23 16:31:45 +0800

    update hadoop 2.7.7 deploy dockerfile

--- hadoop/hadoop-2.7.7-openeuler-deploy/entrypoint.sh
+++ hadoop/hadoop-2.7.7-openeuler-deploy/entrypoint.sh
@@ -1,12 +1,26 @@
 #!/bin/bash -ex
-
 sudo /usr/sbin/sshd
 
-case $1 in
-start)
+NODE_TYPE=${HADOOP_NODE_TYPE:-manual}
+
+case $NODE_TYPE in
+allinone)
   hdfs namenode -format
   start-dfs.sh
   start-yarn.sh
+  ;;
+controller)
+  hdfs namenode -format
+  start-dfs.sh
+  start-yarn.sh
+  ;;
+worker)
+  hadoop-daemon.sh --script hdfs start datanode
+  yarn-daemon.sh start nodemanager
+  ;;
+manual)
+  echo "You need to manually start services!"
+  exec "$@"
   ;;
 *)
   echo "Hadoop All-in-One deployment :)"
