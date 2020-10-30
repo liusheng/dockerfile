6efc95fb40adf9674424aa9614907387a8bd1158
liusheng <liusheng2048@gmail.com>
2020-10-30 15:54:34 +0800

    update

--- hadoop/hadoop-3.3.0-openeuler/entrypoint.sh
+++ hadoop/hadoop-3.3.0-openeuler/entrypoint.sh
@@ -14,7 +14,15 @@ sleep 5
 hostnames=${HOST_NAMES:-""}
 
 for hn in $hostnames; do
-  ipaddr=$(nslookup *.${hn} | grep -Eo '([0-9]+\.)+[0-9]+' | tail -1)
+  ipaddr=""
+  count=0
+  while ! echo $ipaddr | grep "172."
+  do
+    ipaddr=$(nslookup *.${hn} | grep -Eo "([0-9]+\.)+[0-9]+"| tail -1)
+    let count++
+    [ $count -ge 100 ] && echo "ERROR: cannot query internal IP of $hn after 200s" && break
+    sleep 2
+  done
   echo "$ipaddr    $hn" | sudo tee -a /etc/hosts
 done
 
