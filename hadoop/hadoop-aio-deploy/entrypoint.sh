7edd88e7dadf39600dfeea56e80d09d31b4430e6
liusheng <liusheng2048@gmail.com>
2020-12-10 17:15:51 +0800

    update

--- hadoop/hadoop-aio-deploy/entrypoint.sh
+++ hadoop/hadoop-aio-deploy/entrypoint.sh
@@ -1,10 +1,13 @@
 #!/bin/bash -ex
+source ~/.profile
 case $1 in
 start)
   sudo service ssh restart
+  sleep 1
   hdfs namenode -format
   start-dfs.sh
   start-yarn.sh
+  mapred --daemon start historyserver
   ;;
 *)
   echo "Hadoop All-in-One deployment :)"
