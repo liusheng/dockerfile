9fc988c6438b8e5f09657a09c5634c2abc37e1bc
liusheng <liusheng2048@gmail.com>
2020-12-18 16:41:16 +0800

    update git statistic script

--- git-statistic/git-statistic.py
+++ git-statistic/git-statistic.py
@@ -7,6 +7,8 @@ import requests
 from github import Github  # https://pygithub.readthedocs.io/en/latest/examples.html
 from requests.auth import HTTPBasicAuth
 
+CAL_ORGS = ["OpenStack", "Spring Cloud"]
+
 
 def add_cli_args():
     parser = argparse.ArgumentParser()
@@ -33,7 +35,7 @@ def stats_github(repo_name, since, until, token):
     while True:
         print("Query commits stats from github: %s" % url)
         resp = requests.request("GET", url, auth=HTTPBasicAuth('liusheng', token))
-        if resp.status_code == 404:
+        if resp.status_code == 404 or resp.status_code == 409:
             print("NotFound ERROR: %s" % resp.text)
             return 0, 0
         if resp.status_code != 200:
@@ -46,22 +48,49 @@ def stats_github(repo_name, since, until, token):
     return len(contributors), len(set(contributors))
 
 
+def get_org_repos(org, token):
+    res = []
+    url = "https://api.github.com/orgs/%s/repos?per_page=100" % org
+    while True:
+        print("Query repos of %s organization: %s" % (org, url))
+        resp = requests.request("GET", url, auth=HTTPBasicAuth('liusheng', token))
+        if resp.status_code == 404:
+            print("NotFound ERROR: %s" % resp.text)
+            return []
+        if resp.status_code != 200:
+            raise Exception(resp.text)
+        res.extend([r['full_name'] for r in resp.json()])
+        if 'next' not in resp.links:
+            break
+        url = resp.links['next']['url']
+    return res
+
+
 def main():
     parsed_args = add_cli_args().parse_args()
-    row_data = pandas.read_excel("ARM CI Plan.xlsx", sheet_name="Project schedule", engine="openpyxl")
+    row_data = pandas.read_excel("../ARM_CI_Plan.xlsx", sheet_name="Project schedule", engine="openpyxl")
     df = pandas.DataFrame(row_data, columns=["Project", "Repository"])
 
     for row in df.iterrows():
         repo = row[1]["Repository"]
         print("=" * 100)
         print("Start to process project: %s, repo: %s" % (row[1]["Project"], repo))
-        repo = list(filter(lambda s: "github.com" in s, str(repo).splitlines()))
-        if repo:
-            repo = repo[0]
-            repo_name = repo.partition("github.com/")[2].replace(".git", "")
+        repos = list(filter(lambda s: "github.com" in s, str(repo).split()))
+        repos = [repo.partition("github.com/")[2].replace(".git", "").strip('/') for repo in repos]
+        if not repos:
+            continue
+        if str(row[1]["Project"]).strip() in CAL_ORGS:
+            repos = get_org_repos(repos[0], parsed_args.token)
+        else:
+            repos = repos[:1]
+        commits, contributors = 0, 0
+        for repo_name in repos:
             statistics = stats_github(repo_name, parsed_args.since, parsed_args.until, parsed_args.token)
-            df.loc[row[0], "commits_6months"] = statistics[0]
-            df.loc[row[0], "contributors_6months"] = statistics[1]
+            commits += statistics[0]
+            contributors += statistics[1]
+
+        df.loc[row[0], "commits_6months"] = commits
+        df.loc[row[0], "contributors_6months"] = contributors
 
         print("Finish processing project: %s" % row[1]["Project"])
 
